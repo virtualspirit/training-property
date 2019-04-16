9c953f04654762f84be882fd01b092460974224c
Vederis Leunardus <leunardus.vederis714@gmail.com>
2019-04-16 14:57:58 +0800

    Issues #2

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,26 +2,11 @@ class PropertiesController < ApplicationController
   before_action :set_property, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
-  # GET /properties
-  # GET /properties.json
-  def index
-    @properties = Property.all
-  end
-
-  # GET /properties/1
-  # GET /properties/1.json
-  def show
-  end
-
   # GET /properties/new
   def new
     @property = Property.new
     @property.amenities.build
     @property.photos.build
-  end
-
-  # GET /properties/1/edit
-  def edit
   end
 
   # POST /properties
@@ -31,36 +16,11 @@ class PropertiesController < ApplicationController
 
     respond_to do |format|
       if @property.save
-        format.html { redirect_to @property, notice: 'Property was successfully created.' }
-        format.json { render :show, status: :created, location: @property }
+        format.html { redirect_to home_index_url, notice: 'Property was successfully created.' }
       else
         format.html { render :new }
         format.json { render json: @property.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /properties/1
-  # PATCH/PUT /properties/1.json
-  def update
-    respond_to do |format|
-      if @property.update(property_params)
-        format.html { redirect_to @property, notice: 'Property was successfully updated.' }
-        format.json { render :show, status: :ok, location: @property }
-      else
-        format.html { render :edit }
-        format.json { render json: @property.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /properties/1
-  # DELETE /properties/1.json
-  def destroy
-    @property.destroy
-    respond_to do |format|
-      format.html { redirect_to properties_url, notice: 'Property was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
